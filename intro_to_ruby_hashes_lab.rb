755c729845b8509de13b8c9d9421fe913173d074
Leslie Turis <lturis@gmail.com>
2018-09-06 01:18:19 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,7 @@ def monopoly_with_third_tier
 															:names => {
 																:reading_railroad => {
 																	'mortgage_value' => '$100'
-																
+
 																},
 																:pennsylvania_railroad => {
 																	'mortgage_value' => '$200'
